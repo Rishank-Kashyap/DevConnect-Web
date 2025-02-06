62dd8722f5e5bfaab1c4f402b485a0d266ba81dc
Rishank-Kashyap <rishankkashyap03@gmail.com>
2025-02-07 02:31:56 +0530

    Updated ProtectedRoute logic

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,14 +1,14 @@
 import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 
-const ProtectedRoute = ({ children }) => {
+const ProtectedRoute = () => {
   const user = useSelector((store) => store.user);
-  
+
   if (!user) {
     return <Navigate to="/login" replace />;
   }
-  
-  return children;
+
+  return <Outlet />;
 };
 
 export default ProtectedRoute;
